f7c2d969317da2d0042c593b37d05b7a726de2a7
Safiullah Mirzai <sumirzai@gmail.com>
2019-10-16 09:01:20 +0430

    Assignment Done

--- app/src/main/java/com/example/magic8ball/MainActivity.java
+++ app/src/main/java/com/example/magic8ball/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.magic8ball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+ImageView img;
+Button btn;
+int [] imagenode = new int[]{
+        R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5,
 
+};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = findViewById(R.id.imageView);
+        btn = findViewById(R.id.btnask);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int index =   rand.nextInt(5);
+                img.setImageResource(imagenode[index]);
+            }
+        });
     }
 }
